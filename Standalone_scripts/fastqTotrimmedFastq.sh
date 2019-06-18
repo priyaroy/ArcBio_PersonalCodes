91845816f9eacda52b84cc57df82b6100f7f1f94
priyaroy <roy.priyashree@gmail.com>
2019-06-18 16:14:57 -0400

    Add files via upload

---
+++ Standalone_scripts/fastqTotrimmedFastq.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+export pathtolibdir=$1
+export fastp=/mnt/bdbStorage01/programs/fastp/fastp
+cd $pathtolibdir
+
+count=0
+
+for dir in Lib*; do
+    cd $dir;  #Enter each library
+    count=$((count+1))
+    file1=$(find . -maxdepth 1 -name "*_R1_*");
+    file2=$(find . -maxdepth 1 -name "*_R2_*");
+
+    #Remove './' from the filenames of file1 and file2
+    file1=${file1#*/}
+    file2=${file2#*/}
+    trimmed_file1="TRIMMED_R1_$file1"
+    trimmed_file2="TRIMMED_R1_$file2"
+
+    if [ $count -le 2 ]; then
+      echo $fastp
+      echo $file1 
+      echo $file2
+      echo $trimmed_file1
+      echo $trimmed_file2
+      mkdir fastp
+      $fastp -i $file1 -I $file2 -o fastp/$trimmed_file1 -O fastp/$trimmed_file2 > ~/standalone_scripts/logs/log_trimmedFastq 2>~/standalone_scripts/logs/error_trimmedFastq
+    fi
+    cd ..
+done
+
+echo "DONE!"
